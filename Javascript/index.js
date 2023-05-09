2e7d5578fddf93f6c9f338171cc34d5532cd0b4f
samuelkamotho92 <samuelkamotho92@gmail.com>
2023-05-09 11:25:40 +0300

    new updates

---
+++ Javascript/index.js
@@ -0,0 +1,18 @@
+const age = 20
+console.log(typeof age)
+const ageTwo = [20,30,40];
+console.log(ageTwo[ageTwo.length -1]);
+
+const car ={
+    type:'benx',
+    speed:200
+}
+console.log(car.type)
+console.log(car["speed"])
+
+const valOne = 2;
+const valTwo = 4
+4%2
+2%4
+console.log(20%10);
+console.log(10%20);
